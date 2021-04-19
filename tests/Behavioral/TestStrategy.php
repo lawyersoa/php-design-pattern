9b8c75b04184cf2ef0b65f0695ea7134f8399722
qiaoyaqi <tuyashequ@163.com>
2021-04-19 11:18:48 +0800

    initialize

---
+++ tests/Behavioral/TestStrategy.php
@@ -0,0 +1,17 @@
+<?php
+
+/*
+ * This file is part of the lawyersoa/allinpay.
+ *
+ * (c) lawyersoa <business@lawyersoa.com>
+ *
+ * This source file is subject to the MIT license that is bundled
+ * with this source code in the file LICENSE.
+ */
+
+namespace PHPDesignPattern\Pattern\Behavioral\Strategy;
+
+class Strategy
+{
+
+}
